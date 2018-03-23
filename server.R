e0b59c8ca10a88d3e7bdc96296f1845340d2bcc5
sgodlewski <sean.godlewski@gmail.com>
2018-03-23 15:05:05 -0700

    Update server.R

--- server.R
+++ server.R
@@ -6,7 +6,10 @@ library(httr)
 
 url <- "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&json=y&key=MW9S-E7SL-26DU-VV8V"
 
-reactive({
+liveish <- reactive({
     invalidateLater(60000)
     httr::GET(url)
 }) 
+
+
+output <- liveish$root$estimate[4]
